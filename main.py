910b64869c0cdf95724450c0c68844193a5ee349
Takács Alex <takacs.alex@united-consult.hu>
2025-12-05 19:06:36 +0100

    change background color

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
+
+class MainBoard(FloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
 
 class TaskBoard(App):
     def build(self):
-        return super().build()
+        Window.clearcolor = "#393939"
+        return MainBoard()
